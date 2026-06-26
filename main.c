7f275e36dc532830a311d1d36765bb98269e16ae
PabuDev <pablo.sas22@gmail.com>
2026-06-26 20:41:27 -0300

    Movimentação
    Criação da movimentação do robô

---
+++ main.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+// pablo boboca 
+
+#define TAMANHO 20
+
+//Variáveis globais para controle exclusivo pelas funções obrigatórias
+char mundo[TAMANHO][TAMANHO];
+int posicaoRoboX = 19, posicaoRoboY = 19; //Linha X, Coluna Y
+
+//Protótipos das funções abrigatórias
+int moveRobo(int moveX, int moveY);
+
+
+
+int main(){
+    int rodada = 1;
+    int encotrouPremio = 0;
+
+    //Carregar o arquivo texto (mundo)
+    carregarMundo(mundo);
+}
+
+
+//Movimentação do robô para a posição informada
+int moveRobo(int moveX, int moveY){
+    /*Validação 1: Limites da matriz - 
+    Verificar se o robô está dentro do mundo, evitando coordenas além do tamanho da matriz
+    */
+    if (moveX < 0 || moveX >= TAMANHO || moveY < 0 || moveY >= TAMANHO){
+        return 0;
+    }
+
+    //Validação 2: Obstáculo - verificar quando o robô encontra o obstáculo
+    if (mundo[moveX][moveY] == '*'){
+        return 0;
+    }
+
+    //Validação 3: Adjacência (garante que o Robô não se mova na diagonal ou pule casas)
+    int distanciaX = abs(moveX - posicaoRoboX);
+    int distanciaY = abs(moveY - posicaoRoboY);
+    if ((distanciaX + distanciaY) != 1){
+        return 0;
+    }
+
+    //Se passou nas validações, atualiza a posição interna do robô
+    posicaoRoboX = moveX;
+    posicaoRoboY = moveY;
+    return 1;
+
+}
